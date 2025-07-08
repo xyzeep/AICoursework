184d12680d08a8c018c51461b1b821df7d3e073a
pawanacharya73 <pawanacharya832@gmail.com>
2025-07-08 22:30:49 +0545

    data loading, cleaning, and feature selection

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 
+# importing required libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# loading the dataset
+df = pd.read_csv("LifeExpectancyData.csv")
 
 
-print("Hello, world!")
+df.columns = df.columns.str.strip()
 
+# drop colums that don't contribute to prediction
+columns_to_drop = ['Country', 'Year', 'Status']
+df = df.drop(columns=[col for col in columns_to_drop if col in df.columns], errors='ignore')
+
+# checking if there's any missing values
+print(df.isnull().sum())
+
+# droping rows where life expectancy is empty as cannot train with these
+df = df[df['Life expectancy'].notnull()]
+
+# fill other missing values with column means
+df = df.fillna(df.mean(numeric_only=True))
+
+# checking if that worked
+print(df.isnull().sum())
+
+# separating featues and target
+X = df.drop('Life expectancy', axis = 1) # inputs
+y = df['Life expectancy']                # target
+
+# splitting data into training and temp (validation + test) sets
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42) # 0.8 for training, 0.2 for temp (80%, 20%)
+
+X_vali, X_test, y_vali, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42) # 0.5 of 0.2 for each validating and testing (10%)
+
+
+# checking if df contains non-numeric fields and encoding if it does
+print(df.dtypes)
